08890f2448b6f3d6c1238ab64591df6129538da0
Artur G <arturzyk@gmail.com>
2018-09-11 23:01:02 +0200

    route as attribute

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using String = System.String;
 
 namespace Vidly.Controllers
 {
@@ -20,5 +21,28 @@ namespace Vidly.Controllers
             //return new EmptyResult();
             return RedirectToAction("Index", "Home", new {page=1, sortBy="name"});
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
     }
 }
